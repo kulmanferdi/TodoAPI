8e47b0fb79a0c881ad0e21e90db5973674e28159
kulmanferdi <ferdi.kulman@gmail.com>
2025-10-18 01:07:19 +0200

    Todo item test and migration

---
+++ TodoAPI.Tests/TodoItemTests.cs
@@ -0,0 +1,12 @@
+﻿namespace TodoAPI.Tests;
+
+public class TodoItemTests
+{
+    [Fact]
+    public void TodoItem_StoresPropertiesCorrectly()
+    {
+        var todo = new Todo { Id = 1, Title = "Buy milk" };
+        Assert.Equal(1, todo.Id);
+        Assert.Equal("Buy milk", todo.Title);
+    }
+}
