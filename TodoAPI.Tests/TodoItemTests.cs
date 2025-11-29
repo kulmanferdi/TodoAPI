060384b0585e821af035eadc9d099dee24bd270a
kulmanferdi <ferdi.kulman@gmail.com>
2025-11-29 01:23:40 +0100

    New endpoints HTTP - API endpoint test cases one new TodoItem test case [skip ci]

--- TodoAPI.Tests/TodoItemTests.cs
+++ TodoAPI.Tests/TodoItemTests.cs
@@ -9,4 +9,17 @@ public class TodoItemTests
         Assert.Equal(1, todo.Id);
         Assert.Equal("Buy milk", todo.Title);
     }
+
+    [Fact]
+    public void TodoItem_MarkAsComplete_Works()
+    {
+        var todo = new Todo
+        {
+            Id = 1, Title = "Play guitar",
+            IsComplete = false
+        };
+        Assert.False(todo.IsComplete);
+        todo.IsComplete = true;
+        Assert.True(todo.IsComplete);
+    }
 }
--- TodoAPI/Program.cs
+++ TodoAPI/Program.cs
@@ -8,6 +8,34 @@ var app = builder.Build();
 app.MapGet("/todos", async (TodoDb db) =>
     await db.Todos.ToListAsync());
 
+app.MapGet("/todos/completed", async (TodoDb db) =>
+    await db.Todos.Where(t => t.IsComplete).ToListAsync());
+
+app.MapGet("/todos/incomplete", async (TodoDb db) =>
+    await db.Todos.Where(t => !t.IsComplete).ToListAsync());
+
+app.MapGet("/todos/count", async (TodoDb db) =>
+    await db.Todos.CountAsync());
+
+app.MapGet("/todos/count/completed", async (TodoDb db) =>
+    await db.Todos.CountAsync(t => t.IsComplete));
+
+app.MapGet("/todos/count/incomplete", async (TodoDb db) =>
+    await db.Todos.CountAsync(t => !t.IsComplete));
+
+app.MapGet("/todos/search/{term}", async (string term, TodoDb db) =>
+    await db.Todos
+        .Where(t => t.Title.Contains(term))
+        .ToListAsync());
+
+app.MapGet("/todos/ordered/asc", async (TodoDb db) =>
+    await db.Todos.OrderBy(t => t.Title).ToListAsync());
+
+app.MapGet("/todos/ordered/desc", async (TodoDb db) =>
+    await db.Todos
+        .OrderByDescending(t => t.Title)
+        .ToListAsync());
+
 app.MapGet("/todos/{id}", async (int id, TodoDb db) =>
     await db.Todos.FindAsync(id) is { } todo
         ? Results.Ok(todo)
@@ -32,6 +60,16 @@ app.MapPut("/todos/{id}", async (int id, Todo inputTodo, TodoDb db) =>
     return Results.NoContent();
 });
 
+app.MapPatch("/todos/complete/{id}", async (int id, TodoDb db) =>
+{
+    var todo = await db.Todos.FindAsync(id);
+    if (todo is null) return Results.NotFound();
+
+    todo.IsComplete = true;
+    await db.SaveChangesAsync();
+    return Results.Ok(todo);
+});
+
 app.MapDelete("/todos/{id}", async (int id, TodoDb db) =>
 {
     var todo = await db.Todos.FindAsync(id);
@@ -40,6 +78,14 @@ app.MapDelete("/todos/{id}", async (int id, TodoDb db) =>
     db.Todos.Remove(todo);
     await db.SaveChangesAsync();
     return Results.Ok(todo);
+});
+
+app.MapDelete("/todos/completed", async (TodoDb db) =>
+{
+    var completedTodos = db.Todos.Where(t => t.IsComplete);
+    db.Todos.RemoveRange(completedTodos);
+    var deletedCount = await db.SaveChangesAsync();
+    return Results.Ok(new { Deleted = deletedCount });
 });
 
 app.Run();
