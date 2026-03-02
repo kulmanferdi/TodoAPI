2eab0252482fc58ffdc84d4b3e1153d0f786b1ab
kulmanferdi <ferdi.kulman@gmail.com>
2026-03-02 22:57:58 +0100

    Unit tests for title content and separate pipeline for dotnet tests

---
+++ TodoAPI.Tests/ContentTest.cs
@@ -0,0 +1,67 @@
+namespace TodoAPI.Tests;
+
+public class ContentTest
+{
+    [Fact]
+    public Task EmptyTitleTest()
+    {
+        try
+        {
+            var todo = new Todo { Title = "", IsComplete = false };
+            Assert.Equal("", todo.Title);
+            return Task.CompletedTask;
+        }
+        catch (Exception exception)
+        {
+            return Task.FromException(exception);
+        }
+    }
+    
+    [Fact]
+    public Task TitleTest()
+    {
+        try
+        {
+            var todo = new Todo { Title = "Foo", IsComplete = false };
+            Assert.Equal("Foo", todo.Title);
+            return Task.CompletedTask;
+        }
+        catch (Exception exception)
+        {
+            return Task.FromException(exception);
+        }
+    }
+    
+    [Fact]
+    public Task ModifyTitleTest()
+    {
+        try
+        {
+            var todo = new Todo { Title = "Foo", IsComplete = false };
+            Assert.Equal("Foo", todo.Title);
+            todo.Title = "Bar";
+            Assert.Equal("Bar", todo.Title);
+            return Task.CompletedTask;
+        }
+        catch (Exception exception)
+        {
+            return Task.FromException(exception);
+        }
+    }
+    
+    [Fact]
+    public async Task ClearTitleTest()
+    {
+        try
+        {
+            var todo = new Todo { Title = "Foo", IsComplete = false };
+            Assert.Equal("Foo", todo.Title);
+            todo.Title = "";
+            Assert.Equal("", todo.Title);
+        }
+        catch (Exception exception)
+        {
+            await Task.FromException(exception);
+        }
+    }
+}
